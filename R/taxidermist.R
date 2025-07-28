b04d0e8f80bdfde451f2d25b21cf856f5e9ae20d
Pieter Provoost <pieterprovoost@gmail.com>
2025-07-28 22:53:42 +0200

    first commit

---
+++ R/taxidermist.R
@@ -0,0 +1,67 @@
+#' taxidermist
+#'
+#' @docType package
+#' @name taxidermist
+"_PACKAGE"
+
+#' @import dplyr stringr purrr
+NULL
+
+#' Replace string 'NA' with actual NA in all character columns
+#' 
+#' @param df A data frame.
+#' @return A data frame with 'NA' strings in character columns replaced by NA values.
+replace_na_strings <- function(df) {
+  df %>%
+    dplyr::mutate(
+      dplyr::across(
+        where(is.character),
+        ~dplyr::na_if(., "NA")
+      )
+    )
+}
+
+#' Parse taxonomy strings into a data frame
+#'
+#' @param input Character vector. Each element must contain 'tax=' followed by a comma-separated list of key:value pairs (e.g., d:Eukaryota,p:NA,...).
+#' @return A data frame with columns for each taxonomic rank and one row per input string.
+#' @export
+parse_taxonomy_string <- function(input) {
+  ranks_map = c("superkingdom" = "sk", "domain" = "d", "phylum" = "p", "class" = "c", "order" = "o", "family" = "f", "genus" = "g", "species" = "s")
+
+  valid_pattern <- "^.*tax=([a-z]+:[^,]+)(,[a-z]+:[^,]+)*$"
+  if (any(!grepl(valid_pattern, input))) {
+    stop("Each input string must contain 'tax=' followed by a comma-separated list of key:value pairs (e.g., d:Eukaryota,p:NA,...)")
+  }
+
+  input %>%
+    str_extract("(?<=tax=)[^\\s]+") %>% 
+    str_split(",") %>% 
+    map(str_split, ":") %>% 
+    map(function(lst) {
+      setNames(sapply(lst, `[`, 2), sapply(lst, `[`, 1))
+    }) %>% 
+    map(function(lst) {
+      lst[setdiff(unname(ranks_map), names(lst))] <- NA
+      lst[unname(ranks_map)]
+    }) %>%
+    bind_rows() %>% 
+    rename(any_of(ranks_map)) %>%
+    replace_na_strings() %>%
+    mutate(scientificName = do.call(coalesce, across(all_of(rev(names(ranks_map)))))) %>%
+    rowwise() %>%
+    mutate(
+      taxonRank = {
+        vals <- c_across(all_of(names(ranks_map)))
+        non_na <- which(!is.na(vals))
+        if (length(non_na)) names(ranks_map)[max(non_na)] else NA_character_
+      }
+    ) %>%
+    ungroup()
+}
+
+parse_taxonomy <- function(df, col) {
+  col <- rlang::enquo(col)
+  tax_df <- parse_taxonomy_string(pull(df, !!col))
+  dplyr::bind_cols(df, tax_df)
+}
---
+++ tests/testthat/test-all.R
@@ -0,0 +1,41 @@
+library(testthat)
+library(taxidermist)
+
+test_that("parse_taxonomy_string parses strings correctly", {
+  input <- c(
+    ">AF290071;tax=d:Eukaryota,p:NA,c:NA,o:NA,f:NA,g:NA,s:Uncultured_marine_alveolate_Group_II_DH147-EKD16",
+    ">GQ892044;tax=sk:Eukaryota,p:Endomyxa,c:Phytomyxea,o:Plasmodiophorida,f:Plasmodiophoridae,g:Polymyxa,s:Polymyxa_graminis",
+    ">PV277008;tax=d:Eukaryota,p:Annelida,c:Polychaeta,o:NA,f:Capitellidae,g:Notodasus,s:NA",
+    ">XR_012195772;tax=d:NA,p:NA,c:NA,o:NA,f:NA,g:NA,s:NA"
+  )
+  df <- parse_taxonomy_string(input)
+  expect_equal(nrow(df), 4)
+  expect_true(all(c("superkingdom","domain","phylum","class","order","family","genus","species") %in% colnames(df)))
+  expect_equal(df$domain[1], "Eukaryota")
+  expect_equal(df$phylum[2], "Endomyxa")
+  expect_equal(df$scientificName[3], "Notodasus")
+  expect_equal(df$taxonRank[3], "genus")
+  expect_true(is.na(df$phylum[1]))
+  expect_true(is.na(df$superkingdom[1]))
+})
+
+test_that("parse_taxonomy_string converts 'NA' strings to NA", {
+  input <- ">X;tax=d:NA,p:NA,c:NA,o:NA,f:NA,g:NA,s:NA"
+  df <- parse_taxonomy_string(input)
+  expect_true(all(is.na(df)))
+})
+
+test_that("parse_taxonomy_string errors on invalid input", {
+  expect_error(parse_taxonomy_string(">X;d:Eukaryota,p:NA"))
+  expect_error(parse_taxonomy_string(">X;tax=d:Eukaryota,pNA"))
+  expect_error(parse_taxonomy_string(">X;tax=d:Eukaryota,p:NA,c:NA,"))
+})
+
+test_that("parse_taxonomy adds taxonomy columns correctly", {
+  input <- data.frame(taxonomy = c(
+    ">AF290071;tax=d:Eukaryota,p:NA,c:NA,o:NA,f:NA,g:NA,s:Uncultured_marine_alveolate_Group_II_DH147-EKD16",
+    ">GQ892044;tax=sk:Eukaryota,p:Endomyxa,c:Phytomyxea,o:Plasmodiophorida,f:Plasmodiophoridae,g:Polymyxa,s:Polymyxa_graminis"
+  ))
+  df <- input %>% 
+    parse_taxonomy(taxonomy)
+})
